1ce9333fd8926b097ee4317254aeffac9ec104f5
Renato Paduan <renato_paduan@hotmail.com>
2026-02-03 12:41:47 -0300

    33min

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/*
+Lógica de Programação
+- Algoritmo(Receita de bolo - passo a passo)
+
+Fluxo básico
+[] Descobrir quando o botão foi clicado
+[] Pegar o nome da cidade no input
+[] Enviar a cidade para o servidor
+[] Pegar a resposta e colocar na tela
+
+Flúxo de voz
+[] Descobrir quando o botão foi clicado
+[] Começar a ouvir e pegar a transcrição 
+[] Enviar a transcrição para o sevidor 
+[] Pegar a resposta e colocar na tela
+
+Fluxo IA
+[] Pegar os dados da cidade
+[] Enviar dados para a IA
+[] Colocar os dados na tela
+*/
+
